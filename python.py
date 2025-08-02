8d0d170c5eb2333831a1eb33e87e40d89179f768
muqeet-27 <abdulmuqeetkb2006@gmail.com>
2025-08-02 12:57:37 +0530

    added program

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
-print("hello world")
+user = input("Enter your name: ")
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+sum = num1 + num2
+difference = num1 - num2
+print(f"addition = {sum}")
+print(f"difference = {difference}")
+print("Hello, " + user)
